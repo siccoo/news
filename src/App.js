ac9d33804d335bf732b2973a5c17665d4d339b1c
siccoo <obichilaka@gmail.com>
2020-01-23 17:02:37 +0100

    useEffect callback func of setStoryIds strings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+// import {getStoryIds} from './services/hnApi';
 
 export const App = () => {
   const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() =>{
+    setStoryIds('hello')
+  }, []);
+
   return (
-    <p>Hello</p>
+    <p>{storyIds}</p>
   )
 }
