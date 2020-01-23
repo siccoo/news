09c46cf3d37cdcabc6d6d8c011d32e6f2923ec72
siccoo <obichilaka@gmail.com>
2020-01-23 16:40:03 +0100

    new module installed in package.json and calling of some variables on axios

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
-export const baseUrl = "";
+export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newstories.json`;
-export const storyUrl = `${baseUrl}item/`
+export const storyUrl = `${baseUrl}item/`;
+
+export const getStoryIds = () => {
+    const result = await axios.get(newStoriesUrl).then(data => data);
+
+    return result;
+}
