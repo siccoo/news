40c07f99dee707ea75a680efde4353d17e8c8aaa
siccoo <obichilaka@gmail.com>
2020-01-24 11:10:22 +0100

    Modification of the ReadME.md file

--- src/components/Story.js
+++ src/components/Story.js
@@ -8,5 +8,13 @@ export const Story = ({ storyId }) => {
         getStory(storyId).then(source => source && source.url && setStory(source));
     }, []);
 
-return <p>{JSON.stringify(story)}</p>
-}
+return story && story.url ? (
+    <>
+        <a href={story.url}>
+            <p>{story.title}</p>
+        </a>
+        By: <p>{story.by}</p>
+        Posted: <p>{story.time}</p>
+    </>
+    ) : null;
+};
