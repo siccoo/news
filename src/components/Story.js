28db618ee596dadeec90362a45e46957780a531e
siccoo <obichilaka@gmail.com>
2020-01-24 18:04:30 +0100

    Update on Story.js

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getStory } from '../services/hnApi';
-import { StoryWrapper } from '../styles/storyStyles';
+import { StoryWrapper, StoryTitle, StoryMeta, StoryMetaElement } from '../styles/storyStyles';
 
 export const Story = ({ storyId }) => {
     const [story, setStory] = useState({});
@@ -11,11 +11,19 @@ export const Story = ({ storyId }) => {
 
 return story && story.url ? (
     <StoryWrapper datatestid="story">
-        <a href={story.url}>
-            <p>{story.title}</p>
-        </a>
-        By: <p>{story.by}</p>
-        Posted: <p>{story.time}</p>
+        <StoryTitle>
+            <a href={story.url}>{story.title}</a>
+        </StoryTitle>
+
+        <StoryMeta>
+            <span className="story_by" datatestid="story-by">
+                <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+            </span>
+            <span className="story_time" datatestid="story-time">
+                <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+                {story.time}
+            </span>
+        </StoryMeta>
     </StoryWrapper>
     ) : null;
 };
