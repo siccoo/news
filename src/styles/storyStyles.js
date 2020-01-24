d8c95278bfed8f3b64a061ac6cc788c3e785f668
siccoo <obichilaka@gmail.com>
2020-01-24 17:21:30 +0100

    Updates on storyStyles.js

--- src/styles/storyStyles.js
+++ src/styles/storyStyles.js
@@ -36,4 +36,22 @@ export const StoryTitle = styled.h1`
         background-color: #f8dc3d;
         text-decoration: none;
     }
-`
+`;
+
+export const StoryMeta = styled.div`
+    font-style: italic;
+
+    > span:not(:first-child):before {
+        content: '▪';
+        margin: 0 7px;
+    }
+
+    .story_meta-bold {
+        font-weight: bold;
+    }
+`;
+
+export const StoryMetaElement = styled.span`
+    font-weight: bold;
+    color: ${props => props.color || 'red' };
+`;
