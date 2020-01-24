3eb542e217cfab1da30d039710b59b64ed9abe2e
siccoo <obichilaka@gmail.com>
2020-01-24 05:21:51 +0100

    Assigning arrow func getStory variable & result variable variable through axios module

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import {getStoryIds} from '../services/hnApi';
+import { getStoryIds, getStory } from '../services/hnApi';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
-    getStoryIds().then(source => setStoryIds(source));
+    getStoryIds().then(data => setStoryIds(data));
+    getStory(20970623).then(data => console.log(data));
   }, []);
 
-  return (
-    <p>{JSON.stringify(storyIds)}</p>
-  )
+  return <p>{JSON.stringify(storyIds)}</p>;
 }
